30c5c9583b85e6712dc0a12595009bdf2296139f
Silas Windhorst <silaswind1123@gmail.com>
2024-12-04 13:29:52 +0100

    day 4 pt2

--- app/challenges/2024/day4.ts
+++ app/challenges/2024/day4.ts
@@ -1,86 +1,32 @@
 export default function DayFour(inputString: string) {
+  const input = inputString.split("\n").map((x) => x.split(""));
+
+  const checkDirection = (
+    rowIndex: number,
+    cellIndex: number,
+    dRow: number,
+    dCol: number
+  ) => {
+    return (
+      input[rowIndex + dRow]?.[cellIndex + dCol] === "M" &&
+      input[rowIndex + 2 * dRow]?.[cellIndex + 2 * dCol] === "A" &&
+      input[rowIndex + 3 * dRow]?.[cellIndex + 3 * dCol] === "S"
+    );
+  };
+
   const doTheThing = () => {
-    const input = inputString.split("\n").map((x) => x.split(""));
     let result = 0;
     input.forEach((row, rowIndex) => {
-      const spaceTop = rowIndex > 2;
-      const spaceBottom = rowIndex < input.length - 3;
       row.forEach((cell, cellIndex) => {
         if (cell === "X") {
-          const spaceLeft = cellIndex > 2;
-          const spaceRight = cellIndex < row.length - 3;
-          if (spaceTop) {
-            if (
-              input[rowIndex - 1][cellIndex] === "M" &&
-              input[rowIndex - 2][cellIndex] === "A" &&
-              input[rowIndex - 3][cellIndex] === "S"
-            ) {
-              result++;
-            }
-            if (spaceLeft) {
-              if (
-                input[rowIndex - 1][cellIndex - 1] === "M" &&
-                input[rowIndex - 2][cellIndex - 2] === "A" &&
-                input[rowIndex - 3][cellIndex - 3] === "S"
-              ) {
-                result++;
-              }
-            }
-            if (spaceRight) {
-              if (
-                input[rowIndex - 1][cellIndex + 1] === "M" &&
-                input[rowIndex - 2][cellIndex + 2] === "A" &&
-                input[rowIndex - 3][cellIndex + 3] === "S"
-              ) {
-                result++;
-              }
-            }
-          }
-          if (spaceRight) {
-            if (
-              input[rowIndex][cellIndex + 1] === "M" &&
-              input[rowIndex][cellIndex + 2] === "A" &&
-              input[rowIndex][cellIndex + 3] === "S"
-            ) {
-              result++;
-            }
-          }
-          if (spaceBottom) {
-            if (
-              input[rowIndex + 1][cellIndex] === "M" &&
-              input[rowIndex + 2][cellIndex] === "A" &&
-              input[rowIndex + 3][cellIndex] === "S"
-            ) {
-              result++;
-            }
-            if (spaceRight) {
-              if (
-                input[rowIndex + 1][cellIndex + 1] === "M" &&
-                input[rowIndex + 2][cellIndex + 2] === "A" &&
-                input[rowIndex + 3][cellIndex + 3] === "S"
-              ) {
-                result++;
-              }
-            }
-            if (spaceLeft) {
-              if (
-                input[rowIndex + 1][cellIndex - 1] === "M" &&
-                input[rowIndex + 2][cellIndex - 2] === "A" &&
-                input[rowIndex + 3][cellIndex - 3] === "S"
-              ) {
-                result++;
-              }
-            }
-          }
-          if (spaceLeft) {
-            if (
-              input[rowIndex][cellIndex - 1] === "M" &&
-              input[rowIndex][cellIndex - 2] === "A" &&
-              input[rowIndex][cellIndex - 3] === "S"
-            ) {
-              result++;
-            }
-          }
+          if (checkDirection(rowIndex, cellIndex, -1, 0)) result++;
+          if (checkDirection(rowIndex, cellIndex, -1, -1)) result++;
+          if (checkDirection(rowIndex, cellIndex, -1, 1)) result++;
+          if (checkDirection(rowIndex, cellIndex, 0, 1)) result++;
+          if (checkDirection(rowIndex, cellIndex, 1, 0)) result++;
+          if (checkDirection(rowIndex, cellIndex, 1, 1)) result++;
+          if (checkDirection(rowIndex, cellIndex, 1, -1)) result++;
+          if (checkDirection(rowIndex, cellIndex, 0, -1)) result++;
         }
       });
     });
@@ -88,36 +34,33 @@ export default function DayFour(inputString: string) {
   };
 
   const doTheThingPart2 = () => {
-    const input = inputString.split("\n").map((x) => x.split(""));
     let result = 0;
     input.forEach((row, rowIndex) => {
-      const spaceTop = rowIndex > 0;
-      const spaceBottom = rowIndex < input.length - 2;
       row.forEach((cell, cellIndex) => {
-        const spaceLeft = cellIndex > 0;
-        const spaceRight = cellIndex < row.length - 2;
-        if (spaceTop && spaceBottom && spaceLeft && spaceRight) {
-          if (cell === "A") {
-            const topLeft = input[rowIndex - 1][cellIndex - 1];
-            const topRight = input[rowIndex - 1][cellIndex + 1];
-            const bottomLeft = input[rowIndex + 1][cellIndex - 1];
-            const bottomRight = input[rowIndex + 1][cellIndex + 1];
-            if (
-              (topLeft === "M" && bottomRight === "S") ||
-              (topLeft === "S" && bottomRight === "M")
-            ) {
-              if (
-                (topRight === "M" && bottomLeft === "S") ||
-                (topRight === "S" && bottomLeft === "M")
-              ) {
-                result++;
-              }
-            }
+        if (
+          cell === "A" &&
+          rowIndex > 0 &&
+          rowIndex < input.length - 1 &&
+          cellIndex > 0 &&
+          cellIndex < row.length - 1
+        ) {
+          const topLeft = input[rowIndex - 1][cellIndex - 1];
+          const topRight = input[rowIndex - 1][cellIndex + 1];
+          const bottomLeft = input[rowIndex + 1][cellIndex - 1];
+          const bottomRight = input[rowIndex + 1][cellIndex + 1];
+          if (
+            ((topLeft === "M" && bottomRight === "S") ||
+              (topLeft === "S" && bottomRight === "M")) &&
+            ((topRight === "M" && bottomLeft === "S") ||
+              (topRight === "S" && bottomLeft === "M"))
+          ) {
+            result++;
           }
         }
       });
     });
     return result;
   };
+
   return { doTheThing, doTheThingPart2 };
 }
