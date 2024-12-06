981944e52dcecc16ee0222d5b97c559708d83cc5
Silas Windhorst <silas.windhorst@tuhh.de>
2024-12-06 12:39:05 +0100

    day6 p1 continued

--- app/challenges/2024/day6.ts
+++ app/challenges/2024/day6.ts
@@ -1,6 +1,78 @@
 export default function DaySix(inputString: string) {
+  const formattedInput = inputString.split("\n").map(e => e.split(""));
   const doTheThing = () => {
-    let result = 0;
+    let result: number[][] = [];
+    let pos = [0, 0];
+    for (let i = 0; i < formattedInput.length; i++) {
+      const index = formattedInput[i].findIndex(e => e !== "." && e !== "#");
+      if (index !== -1) {
+        pos = [i, index];
+        break;
+      }
+    }
+    const guard = formattedInput[pos[0]][pos[1]];
+    let direction =
+      guard === "^"
+        ? "up"
+        : guard === ">"
+        ? "right"
+        : guard === "<"
+        ? "left"
+        : "down";
+    let done = false;
+    while (!done) {
+      result.push(pos);
+      if (direction === "up") {
+        const nextPoint = formattedInput[pos[0] + 1][pos[1]];
+        if (pos[0] === 0) {
+          done = true;
+          break;
+        }
+        if (nextPoint === ".") {
+          pos[0]--;
+        } else {
+          direction = "right";
+          pos[1]++;
+        }
+      } else if (direction === "right") {
+        const nextPoint = formattedInput[pos[0]][pos[1] + 1];
+        if (pos[1] === formattedInput[0].length - 1) {
+          done = true;
+          break;
+        }
+        if (nextPoint === ".") {
+          pos[1]++;
+        } else {
+          direction = "down";
+          pos[0]++;
+        }
+      } else if (direction === "down") {
+        const nextPoint = formattedInput[pos[0] - 1][pos[1]];
+        if (pos[0] === formattedInput.length - 1) {
+          done = true;
+          break;
+        }
+        if (nextPoint === ".") {
+          pos[0]++;
+        } else {
+          direction = "left";
+          pos[1]--;
+        }
+      } else {
+        const nextPoint = formattedInput[pos[0]][pos[1] - 1];
+        if (pos[1] === 0) {
+          done = true;
+          break;
+        }
+        if (nextPoint === ".") {
+          pos[1]--;
+        } else {
+          direction = "up";
+          pos[0]--;
+        }
+      }
+    }
+
     return result;
   };
 
