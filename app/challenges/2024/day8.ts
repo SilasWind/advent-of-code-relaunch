d767516d1469066e00870c03b42f6dddd7dfda5e
Silas Windhorst <silas.windhorst@tuhh.de>
2024-12-08 12:18:09 +0100

    day 8 stub

--- app/challenges/2024/day8.ts
+++ app/challenges/2024/day8.ts
@@ -1,6 +1,10 @@
 export default function DayEight(inputString: string) {
+  const formattedInput = inputString.split("\n").map(e => e.split(""));
   const doTheThing = () => {
     let result = 0;
+    formattedInput.forEach((row, index) => {
+      row.forEach((char, i) => {});
+    });
     return result;
   };
 
